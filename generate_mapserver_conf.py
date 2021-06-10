47417d527f238c0510f500ab576ff39b36b93157
Nick Young <nick.young@auckland.ac.nz>
2021-06-10 13:18:12 +1200

    script to generate mapserver.map file

---
+++ generate_mapserver_conf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from glob import glob
+from os.path import basename, splitext
+files = glob("/mnt/Hauturu/*/*.tif")
+
+print("""MAP
+    PROJECTION
+        "init=epsg:3857" # Output projection
+    END""")
+
+for filepath in files:
+    name = splitext(basename(filepath))[0]
+    print(f"""
+    LAYER
+        NAME "{name}"
+        DATA "{filepath}"
+        TYPE RASTER
+        PROJECTION
+            "init=epsg:4326"
+        END
+        STATUS ON
+    END""")
+print("""
+    WEB
+        METADATA
+            "wms_title" "Hauturu imagery"
+            "wms_enable_request"  "*"
+        END
+    END
+END
+""")
